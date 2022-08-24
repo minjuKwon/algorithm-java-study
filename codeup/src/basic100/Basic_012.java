7cc77a1f3f3d6c34a2f15a633e1308b6d30853ed
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-08-24 19:06:07 +0900

    Codeup Basic012
    study codeup algorithm : Basic012

---
+++ codeup/src/basic100/Basic_012.java
@@ -0,0 +1,17 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Basic_012 {
+
+	public static void main(String[] args) {
+		int a,b;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		b=s.nextInt();
+		System.out.println(a+" "+b);
+		s.close();
+
+	}
+
+}
