c6233e0545e2dec9aad7991cacb5a709c9ea0984
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-08-24 18:30:28 +0900

    Codeup Basic008
    study codeup algorithm : Basic008

---
+++ codeup/src/basic100/Basic_008.java
@@ -0,0 +1,11 @@
+package basic100;
+
+public class Basic_008 {
+
+	public static void main(String[] args) {
+		System.out.println("\u250C\u252C\u2510\n"
+				+ "\u251C\u253C\u2524\n"
+				+ "\u2514\u2534\u2518");
+	}
+
+}
