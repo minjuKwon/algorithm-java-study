07dc8e07b0a93548ded1f51df733e6f5ad78e5ef
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-08-26 23:48:10 +0900

    Codeup Basic019
    study codeup algorithm : Basic019

---
+++ codeup/src/basic100/Basic_019.java
@@ -0,0 +1,21 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Basic_019 {
+
+	public static void main(String[] args) {
+		
+		String word;
+		
+		Scanner scanner=new Scanner(System.in);
+		
+		word=scanner.next();
+		
+		System.out.println(word);
+		
+		scanner.close();
+
+	}
+
+}
