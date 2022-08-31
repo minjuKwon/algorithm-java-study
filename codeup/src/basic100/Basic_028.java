eb037a7873fdb46e6f111ef716afbb242650ab48
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-08-31 19:06:18 +0900

    Codeup Basic028
    study codeup algorithm : Basic028

---
+++ codeup/src/basic100/Basic_028.java
@@ -0,0 +1,21 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Basic_028 {
+
+	public static void main(String[] args) {
+
+		long n;
+		
+		Scanner scanner=new Scanner(System.in);
+		
+		n=scanner.nextLong();
+		
+		System.out.println(n);
+		
+		scanner.close();
+
+	}
+
+}
