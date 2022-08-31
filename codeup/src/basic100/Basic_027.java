b3dc77454ebde796ed22d3ecc5dba40047882a1b
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-08-31 18:38:36 +0900

    Codeup Basic027
    study codeup algorithm : Basic027

---
+++ codeup/src/basic100/Basic_027.java
@@ -0,0 +1,21 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Basic_027 {
+
+	public static void main(String[] args) {
+		
+		double d;
+		
+		Scanner scanner=new Scanner(System.in);
+		
+		d=scanner.nextDouble();
+		
+		System.out.printf("%.11f",d);
+		
+		scanner.close();
+
+	}
+
+}
