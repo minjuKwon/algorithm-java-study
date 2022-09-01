c5914c379328827d9d933fefb65d5965c9feea49
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-09-01 20:16:52 +0900

    Codeup Basic030
    study codeup algorithm : Basic030

---
+++ codeup/src/basic100/Basic_030.java
@@ -0,0 +1,21 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Basic_030 {
+
+	public static void main(String[] args) {
+		int a;
+		
+		Scanner scanner=new Scanner(System.in);
+		
+		a=scanner.nextInt();
+		
+		System.out.printf("%x",a);
+		
+		scanner.close();
+
+	}
+
+}
+ 
