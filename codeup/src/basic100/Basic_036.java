bbdc93412752598333d0afad6a2cab50a3a17eba
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-09-02 20:57:48 +0900

    Codeup Basic036
    study codeup algorithm : Basic036

---
+++ codeup/src/basic100/Basic_036.java
@@ -0,0 +1,22 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Basic_036 {
+
+	public static void main(String[] args) {
+		
+		long num1,num2,sum;
+		
+		Scanner scanner=new Scanner(System.in);
+		num1=scanner.nextLong();
+		num2=scanner.nextLong();
+		scanner.close();
+		
+		sum=num1+num2;
+		
+		System.out.println(sum);
+		
+	}
+
+}
