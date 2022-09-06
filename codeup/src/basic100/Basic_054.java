79c96128361b82fe2dff0e51038425f7e65e6fa6
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-09-06 20:48:44 +0900

    Codeup Basic054
    study codeup algorithm : Basic054

---
+++ codeup/src/basic100/Basic_054.java
@@ -0,0 +1,24 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Basic_054 {
+
+	public static void main(String[] args) {
+		
+		int num1,num2;
+		boolean a,b;
+		
+		Scanner scanner=new Scanner(System.in);
+		num1=scanner.nextInt();
+		num2=scanner.nextInt();
+		scanner.close();
+		
+		a = num1==0 ? false : true;
+		b = num2==0 ? false : true;
+		
+		System.out.println((a&&!b)||(!a&&b) ? 1 : 0);
+
+	}
+
+}
