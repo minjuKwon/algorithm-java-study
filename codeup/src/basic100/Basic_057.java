52ea9c2962aa3e0c4091242c3b01acb7d62f792a
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-09-07 18:02:08 +0900

    Codeup Basic057
    study codeup algorithm : Basic057

---
+++ codeup/src/basic100/Basic_057.java
@@ -0,0 +1,19 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Basic_057 {
+
+	public static void main(String[] args) {
+		
+		int a;
+		
+		Scanner scanner=new Scanner(System.in);
+		a=scanner.nextInt();
+		scanner.close();
+		
+		System.out.println(~a);
+
+	}
+
+}
