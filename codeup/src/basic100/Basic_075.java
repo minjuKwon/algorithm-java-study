62986cf9a15ba405ce745f0755107446c3895905
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-09-09 00:24:54 +0900

    Codeup Basic075
    study codeup algorithm : Basic075

---
+++ codeup/src/basic100/Basic_075.java
@@ -0,0 +1,21 @@
+package basic100;
+
+import java.util.Scanner;
+
+public class Basic_075 {
+
+	public static void main(String[] args) {
+		
+		int a;
+		
+		Scanner scanner=new Scanner(System.in);
+		a=scanner.nextInt();
+		scanner.close();
+		
+		for(int i=0;i<=a;i++){
+		    System.out.println(i);
+		}
+
+	}
+
+}
