51ad0c4c77430c2dc4cc93a91f01972f1a472b1f
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-10-01 16:24:56 +0900

    Baekjoon Step01_06
    study baekjoon algorithm : Step01_06

---
+++ baekjoon/src/step1to10/Step_01_06.java
@@ -0,0 +1,18 @@
+package step1to10;
+
+import java.util.Scanner;
+
+public class Step_01_06 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		double A=sc.nextDouble();
+		double B=sc.nextDouble();
+		sc.close();
+
+		System.out.println(A/B);
+
+	}
+
+}
