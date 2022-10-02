7b0d7361498c731642d7db01ee0fa06d26ed89fb
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-10-02 19:51:35 +0900

    Baekjoon Step01_08
    study baekjoon algorithm : Step01_08

---
+++ baekjoon/src/step1to10/Step_01_08.java
@@ -0,0 +1,17 @@
+package step1to10;
+
+import java.util.Scanner;
+
+public class Step_01_08 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		String name=sc.next();
+		sc.close();
+		
+		System.out.println(name+"??!");
+
+	}
+
+}
