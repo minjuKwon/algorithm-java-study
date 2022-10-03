588e9237ffc74a10830bf9b8448188eba0572740
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-10-03 18:34:35 +0900

    Baekjoon Step01_14
    study baekjoon algorithm : Step01_14

---
+++ baekjoon/src/step1to10/Step_01_14.java
@@ -0,0 +1,15 @@
+package step1to10;
+
+public class Step_01_14 {
+
+	public static void main(String[] args) {
+		
+		System.out.print("|\\_/|\n" + 
+				"|q p|   /}\n" + 
+				"( 0 )\"\"\"\\\n" + 
+				"|\"^\"`    |\n" + 
+				"||_/=\\\\__|");
+
+	}
+
+}
