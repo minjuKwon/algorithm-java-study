a0636b48344b4d69b23e06a8d82451e22c0dce0d
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-10-03 18:15:29 +0900

    Baekjoon Step01_11
    study baekjoon algorithm : Step01_11

---
+++ baekjoon/src/step1to10/Step_01_11.java
@@ -0,0 +1,22 @@
+package step1to10;
+
+import java.util.Scanner;
+
+public class Step_01_11 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		sc.close();
+
+		System.out.println((A + B) % C);
+		System.out.println(((A % C) + (B % C)) % C);
+		System.out.println((A * B) % C);
+		System.out.println(((A % C) * (B % C)) % C);
+
+	}
+
+}
