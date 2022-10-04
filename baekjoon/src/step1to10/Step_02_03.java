12e1aa04bedc6e04bf7313d81bad6059ad9b9de0
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-10-04 20:41:22 +0900

    Baekjoon Step02_03
    study baekjoon algorithm : Step02_03

---
+++ baekjoon/src/step1to10/Step_02_03.java
@@ -0,0 +1,20 @@
+package step1to10;
+
+import java.util.Scanner;
+
+public class Step_02_03 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int year=sc.nextInt();
+		sc.close();
+		
+		if(year%4==0&&year%100!=0||year%400==0)
+			System.out.println(1);
+		else
+		System.out.println(0);
+
+	}
+
+}
