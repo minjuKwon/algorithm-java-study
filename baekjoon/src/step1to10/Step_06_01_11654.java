23a90cba061a5f91333a36d8217e8771eafaa807
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-11-11 19:38:03 +0900

    Baekjoon Step06_01_11654
    study baekjoon algorithm : Step06_01_11654

---
+++ baekjoon/src/step1to10/Step_06_01_11654.java
@@ -0,0 +1,20 @@
+package step1to10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Step_06_01_11654 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		char input=br.readLine().charAt(0);
+		int ASCII=input;
+		
+		System.out.println(ASCII);
+		
+	}
+
+}
