f78b7c3950dc7eaea76ccc81c1b431bb32ecfa08
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-11-30 17:58:41 +0900

    Baekjoon Step07_05_2775_02
    study baekjoon algorithm : Step07_05_2775_02

---
+++ baekjoon/src/step1to10/Step_07_05_2775_02.java
@@ -0,0 +1,39 @@
+package step1to10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//출처 : https://st-lab.tistory.com/78
+public class Step_07_05_2775_02 {
+	
+	public static int [][] APT=new int[15][15];
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb=new StringBuilder();
+		
+		make_APT();
+		
+		int T=Integer.parseInt(br.readLine());
+		for(int i=0;i<T;i++) {
+			int k=Integer.parseInt(br.readLine());
+			int n=Integer.parseInt(br.readLine());
+			sb.append(APT[k][n]).append('\n');
+		}
+		System.out.println(sb);
+	}
+	
+	public static void make_APT() {
+		for(int i=0;i<15;i++) {
+			APT[i][1]=1;
+			APT[0][i]=i;
+		}
+		for(int i=1;i<15;i++) {
+			for(int j=2;j<15;j++) {
+				APT[i][j]=APT[i][j-1]+APT[i-1][j];
+			}
+		}
+	}
+
+}
