1377f4ebdad7a80a4db871395daf4ff6b0d6c57c
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-12-01 19:04:32 +0900

    Baekjoon Step07_07_2839
    study baekjoon algorithm : Step07_07_2839

---
+++ baekjoon/src/step1to10/Step_07_07_2839.java
@@ -0,0 +1,28 @@
+package step1to10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//출처 : https://st-lab.tistory.com/72
+public class Step_07_07_2839 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int n=Integer.parseInt(br.readLine());
+		
+		if(n==4||n==7) {
+			System.out.println(-1);
+		}
+		else if(n%5==0) {
+			System.out.println(n/5);
+		}else if(n%5==1||n%5==3) {
+			System.out.println((n/5)+1);
+		}
+		else if(n%5==2||n%5==4) {
+			System.out.println((n/5)+2);
+		}
+		
+	}
+
+}
