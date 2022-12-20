6de5b136ac1cc1f15c4ac2a6cd0df55ad3e3b67e
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2022-12-20 11:43:04 +0900

    Baekjoon Step10_01_2750_04
    study baekjoon algorithm : Step10_01_2750_04

---
+++ baekjoon/src/step1to10/Step_10_01_2750_04.java
@@ -0,0 +1,29 @@
+package step1to10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Step_10_01_2750_04 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(br.readLine());
+		
+		int [] arr=new int [N];
+		for(int i=0;i<arr.length;i++)
+			arr[i]=Integer.parseInt(br.readLine());
+		
+		Arrays.sort(arr);
+		
+		StringBuilder sb=new StringBuilder();
+		for(int num:arr)
+			sb.append(num).append('\n');
+		
+		System.out.println(sb);
+
+	}
+
+}
