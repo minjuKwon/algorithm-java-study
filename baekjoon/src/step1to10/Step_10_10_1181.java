beb50b3824fb21790a9f26df528662e30ac41ef5
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-01-09 20:36:07 +0900

    Rename file Step_10_11_1181 to Step_10_10_1181

--- baekjoon/src/step1to10/Step_10_10_1181.java
+++ baekjoon/src/step1to10/Step_10_10_1181.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Step_10_11_1181 {
+public class Step_10_10_1181 {
 
 	public static void main(String[] args) throws IOException{
 		
