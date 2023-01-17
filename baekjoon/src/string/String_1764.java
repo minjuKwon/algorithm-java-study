a3e1b3ac1730cbfe2446f03d8f57410148643b77
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-01-17 21:31:06 +0900

    Update String_1764.java

--- baekjoon/src/string/String_1764.java
+++ baekjoon/src/string/String_1764.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class String_1764 {
@@ -18,27 +18,26 @@ public class String_1764 {
 		int N=Integer.parseInt(st.nextToken());
 		int M=Integer.parseInt(st.nextToken());
 		
-		HashMap<String,Integer> map=new HashMap<>(N);
+		HashSet<String> set=new HashSet<>();
 		for(int i=0;i<N;i++)
-			map.put(br.readLine(), 0);
-		
-		int count=0;
+			set.add(br.readLine());
+
 		ArrayList<String> str=new ArrayList<>();
 		for(int i=0;i<M;i++) {
 			String m=br.readLine();
-			if(map.containsKey(m)) {
+			if(set.contains(m)) {
 				str.add(m);
-				count++;
 			}
 		}
 		
 		str.sort(Comparator.naturalOrder());
 		
 		StringBuilder sb=new StringBuilder();
+		sb.append(str.size()).append('\n');
 		for(String s:str)
 			sb.append(s).append('\n');
 		
-		System.out.println(count+"\n"+sb);
+		System.out.println(sb);
 		
 	}
 
