193a1fbe3d295f4cb3004e179cedaab889401811
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-02-06 19:10:48 +0900

    Baekjoon Step13_06_1269_02
    study baekjoon algorithm : Step13_06_1269_02

---
+++ baekjoon/src/step11to20/Step_13_06_1269_02.java
@@ -0,0 +1,44 @@
+package step11to20;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Step_13_06_1269_02 {
+
+	public static void main(String[] args)throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stN=new StringTokenizer(br.readLine());
+		
+		int numA=Integer.parseInt(stN.nextToken());
+		int numB=Integer.parseInt(stN.nextToken());
+		
+		HashSet<Integer> setA=new HashSet<>();
+		HashSet<Integer> setB=new HashSet<>();
+		
+		StringTokenizer stA=new StringTokenizer(br.readLine());
+		for(int i=0;i<numA;i++)
+			setA.add(Integer.parseInt(stA.nextToken()));
+		
+		StringTokenizer stB=new StringTokenizer(br.readLine());
+		for(int i=0;i<numB;i++)
+			setB.add(Integer.parseInt(stB.nextToken()));
+		
+		int count=0;
+		for(int i:setA) {
+			if(!setB.contains(i))
+				count++;
+		}
+		for(int i:setB) {
+			if(!setA.contains(i))
+				count++;
+		}
+		
+		System.out.println(count);
+
+	}
+
+}
