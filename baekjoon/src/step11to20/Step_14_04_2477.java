1aa2e653a23912ea25fcd5f5ae250687b6477489
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-02-09 20:36:18 +0900

    Modify import library Baekjoon Step14_04_2477

--- baekjoon/src/step11to20/Step_14_04_2477.java
+++ baekjoon/src/step11to20/Step_14_04_2477.java
@@ -3,7 +3,6 @@ package step11to20;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Step_14_04_2477 {
@@ -20,6 +19,7 @@ public class Step_14_04_2477 {
 		
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		int K=Integer.parseInt(br.readLine());
+		
 		for(int i=0;i<6;i++) {
 			StringTokenizer st=new StringTokenizer(br.readLine());
 			int d=Integer.parseInt(st.nextToken());
