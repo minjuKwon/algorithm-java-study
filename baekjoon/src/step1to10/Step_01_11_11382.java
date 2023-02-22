c97b6f5e872f7bc5b29ff672e87c46481ae9e2a6
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-02-22 13:41:43 +0900

    Baekjoon Step01_11_11382
    study baekjoon algorithm : Step01_11_11382

---
+++ baekjoon/src/step1to10/Step_01_11_11382.java
@@ -0,0 +1,23 @@
+package step1to10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Step_01_11_11382 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		long A=Long.parseLong(st.nextToken());
+		long B=Long.parseLong(st.nextToken());
+		long C=Long.parseLong(st.nextToken());
+		
+		System.out.println(A+B+C);
+
+	}
+
+}
