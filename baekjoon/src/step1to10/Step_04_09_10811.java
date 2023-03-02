c85cf36ac80909fa305ed7fcb080ad22996071cb
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-03-02 15:59:21 +0900

    Baekjoon Step04_09_10811
    study baekjoon algorithm : Step04_09_10811

---
+++ baekjoon/src/step1to10/Step_04_09_10811.java
@@ -0,0 +1,49 @@
+package step1to10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Step_04_09_10811 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		int N=Integer.parseInt(st.nextToken());
+		int M=Integer.parseInt(st.nextToken());
+		
+		int[] arr=new int[N];
+		for(int i=0;i<N;i++) {
+			arr[i]=i+1;
+		}
+		
+		StringTokenizer stNum;
+		while(M-->0) {
+			stNum=new StringTokenizer(br.readLine());
+			int i=Integer.parseInt(stNum.nextToken());
+			int j=Integer.parseInt(stNum.nextToken());
+			
+			int temp=0;
+			while(j>i) {
+				temp=arr[j-1];
+				arr[j-1]=arr[i-1];
+				arr[i-1]=temp;
+				i++;j--;
+			}
+		}
+		
+		br.close();
+		
+		StringBuilder sb=new StringBuilder();
+		for(int num:arr) {
+			sb.append(num).append(' ');
+		}
+		
+		System.out.println(sb);
+
+	}
+
+}
