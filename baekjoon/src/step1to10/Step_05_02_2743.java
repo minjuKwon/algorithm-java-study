36145f40d8b1a3ffdb3b6aea2e3608e9856e0f60
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-03-04 13:55:34 +0900

    Baekjoon Step05_02_2743
    study baekjoon algorithm : Step05_02_2743

---
+++ baekjoon/src/step1to10/Step_05_02_2743.java
@@ -0,0 +1,19 @@
+package step1to10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Step_05_02_2743 {
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String str=br.readLine();
+		br.close();
+		
+		System.out.println(str.length());
+		
+	}
+
+}
