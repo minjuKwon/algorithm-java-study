a88981c150194abe9b4b780f7520938b24546cc5
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-04-13 17:10:07 +0900

    Baekjoon DynamicProgramming_13699
    study baekjoon algorithm : DynamicProgramming_13699

---
+++ baekjoon/src/dynamicProgramming/DynamicProgramming_13699.java
@@ -0,0 +1,33 @@
+package dynamicProgramming;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DynamicProgramming_13699 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int n=Integer.parseInt(br.readLine());
+		
+		br.close();
+
+		long [] dp=new long[n+1];
+		
+		dp[0]=1;
+		if(n>1)
+			dp[1]=1;
+		
+		for(int i=2;i<=n;i++) {
+			for(int j=0;j<i;j++) {
+				dp[i]+=dp[j]*dp[i-j-1];
+			}
+		}
+		
+		System.out.println(dp[n]);
+
+	}
+
+}
