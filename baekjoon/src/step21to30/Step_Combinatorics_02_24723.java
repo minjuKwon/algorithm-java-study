417f513265be334583ec2bbbd0d3c82477bb10a9
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-04-14 14:42:53 +0900

    Baekjoon Step_Combinatorics_02_24723
    study baekjoon algorithm : Step_Combinatorics_02_24723

---
+++ baekjoon/src/step21to30/Step_Combinatorics_02_24723.java
@@ -0,0 +1,21 @@
+package step21to30;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Step_Combinatorics_02_24723 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int N=Integer.parseInt(br.readLine());
+		
+		br.close();
+		
+		System.out.println((int)Math.pow(2, N));
+
+	}
+
+}
