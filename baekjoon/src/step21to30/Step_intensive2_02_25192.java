5c8df62b508f8e06384145698577ceea55eb51e3
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-04-14 18:47:54 +0900

    Baekjoon Step_intensive2_02_25192
    study baekjoon algorithm : Step_intensive2_02_25192

---
+++ baekjoon/src/step21to30/Step_intensive2_02_25192.java
@@ -0,0 +1,35 @@
+package step21to30;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Step_intensive2_02_25192 {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int N=Integer.parseInt(br.readLine());
+		
+		int count=0;
+		
+		HashSet<String> set=new HashSet<String>();
+		while(N-->0) {
+			String user=br.readLine();
+			if(user.equals("ENTER")) {
+				count+=set.size();
+				set.clear();
+			}else {
+				set.add(user);				
+			}
+		}
+		
+		count+=set.size();
+		
+		System.out.println(count);
+
+	}
+
+}
