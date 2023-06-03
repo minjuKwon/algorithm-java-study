8de7c63f163608c4595f9e83c4f69c697d64d567
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-06-03 17:31:24 +0900

    Baekjoon Step_PriorityQueue_01_11279_03
    study baekjoon algorithm : Step_PriorityQueue_01_11279_03

---
+++ baekjoon/src/step21to30/Step_PriorityQueue_01_11279_03.java
@@ -0,0 +1,39 @@
+package step21to30;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Step_PriorityQueue_01_11279_03 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int N=Integer.parseInt(br.readLine());
+		
+		PriorityQueue <Integer> queue=new PriorityQueue<>(Collections.reverseOrder());
+		StringBuilder sb=new StringBuilder();
+		
+		while(N-->0) {
+			int x=Integer.parseInt(br.readLine());
+			if(x==0) {
+				if(queue.isEmpty()) {
+					sb.append(0).append('\n');
+				}else {
+					sb.append(queue.poll()).append('\n');
+				}
+			}else {
+				queue.add(x);
+			}
+		}
+		
+		br.close();
+		
+		System.out.println(sb);
+		
+	}
+
+}
