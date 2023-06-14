8cfb960b7a8220e3cb5d138d18fc548a8b1071eb
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-06-14 15:25:37 +0900

    Baekjoon Step_Stack2_02_17298
    study baekjoon algorithm : Step_Stack2_02_17298

---
+++ baekjoon/src/step21to30/Step_Stack2_02_17298.java
@@ -0,0 +1,48 @@
+package step21to30;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+//https://st-lab.tistory.com/196
+public class Step_Stack2_02_17298 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int N=Integer.parseInt(br.readLine());
+		
+		int [] arr=new int [N];
+		
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		br.close();
+		
+		Stack<Integer> stack=new Stack<>();
+		
+		for(int i=0;i<N;i++) {
+			while(!stack.isEmpty()&&arr[i]>arr[stack.peek()]) {
+				arr[stack.pop()]=arr[i];
+			}
+			stack.push(i);
+		}
+		
+		while(!stack.isEmpty()) {
+			arr[stack.pop()]=-1;
+		}
+		
+		StringBuilder sb=new StringBuilder();
+		for(int a:arr) {
+			sb.append(a).append(' ');
+		}
+
+		System.out.println(sb);
+		
+	}
+
+}
