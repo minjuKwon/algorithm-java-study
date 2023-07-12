c5f3ca09a7c42406407d2872a6fd8d83e430b0ec
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-07-12 16:20:31 +0900

    Baekjoon Step_TwoPointers_01_3273
    study baekjoon algorithm : Step_TwoPointers_01_3273

---
+++ baekjoon/src/step31to40/Step_TwoPointers_01_3273.java
@@ -0,0 +1,47 @@
+package step31to40;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Step_TwoPointers_01_3273 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int n=Integer.parseInt(br.readLine());
+	
+		int [] arr=new int[n];
+		
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		for(int i=0;i<n;i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		int x=Integer.parseInt(br.readLine());
+		
+		br.close();
+		
+		Arrays.sort(arr);
+		
+		int start = 0; int end=n-1;
+		int sum=0; int count=0;
+		
+		while(start<end) {
+			sum=arr[start]+arr[end];
+			if(sum==x) {count++;}
+			if(sum<=x) {
+				start++;
+			}else {
+				end--;
+			}
+		}
+		
+		System.out.println(count);
+
+	}
+
+}
