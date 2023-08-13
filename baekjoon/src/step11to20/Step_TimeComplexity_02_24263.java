a7685c849bdd7e51fe1137e000f3dde47379649d
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-08-13 18:07:19 +0900

    Baekjoon Step_TimeComplexity_02_24263
    study baekjoon algorithm : Step_TimeComplexity_02_24263

---
+++ baekjoon/src/step11to20/Step_TimeComplexity_02_24263.java
@@ -0,0 +1,17 @@
+package step11to20;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Step_TimeComplexity_02_24263 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(br.readLine()+"\n"+1);
+
+	}
+
+}
