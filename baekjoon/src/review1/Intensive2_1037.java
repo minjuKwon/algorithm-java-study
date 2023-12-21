1ffd110d9bfd592d06407e4152cd0ccbae839b75
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-12-21 11:36:05 +0900

    Baekjoon Intensive2_1037
    study baekjoon algorithm : Intensive2_1037

---
+++ baekjoon/src/review1/Intensive2_1037.java
@@ -0,0 +1,31 @@
+package review1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Intensive2_1037 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(br.readLine());
+		
+		int [] arr=new int[N];
+		
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+		
+		br.close();
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[0]*arr[N-1]);
+		
+	}
+
+}
