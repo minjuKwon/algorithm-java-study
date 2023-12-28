bf72515cb113884423dd8a7299f56d46ee716d91
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2023-12-28 13:01:31 +0900

    Baekjoon Intensive2_20920
    study baekjoon algorithm : Intensive2_20920

---
+++ baekjoon/src/review1/Intensive2_20920.java
@@ -0,0 +1,56 @@
+package review1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Intensive2_20920 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		int N=Integer.parseInt(st.nextToken());
+		int M=Integer.parseInt(st.nextToken());
+		
+		HashMap<String, Integer> map=new HashMap<>();
+
+		while(N-->0) {
+			String word=br.readLine();
+			if(word.length()>=M) {
+				map.put(word, map.getOrDefault(word, 0)+1);
+			}
+		}
+		
+		br.close();
+		
+		ArrayList<String> list=new ArrayList<>(map.keySet());
+		
+		list.sort((o1, o2)->{
+			int v1=map.get(o1);
+			int v2=map.get(o2);
+			if(v1==v2) {
+				if(o1.length()==o2.length()) {
+					return o1.compareTo(o2);
+				}
+				return o2.length()-o1.length();
+			}
+			return v2-v1;
+			}
+	   );
+		
+		StringBuilder sb=new StringBuilder();
+		
+		for(String word:list) {
+			sb.append(word).append('\n');
+		}
+		
+		System.out.println(sb);
+		
+	}
+
+}
