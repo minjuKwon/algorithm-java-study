082ff47caed5d1d5581e926e3038da9ddbc7fb77
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2024-03-19 00:25:45 +0900

    Baekjoon BruteForce_1436
    study baekjoon algorithm : BruteForce_1436

---
+++ baekjoon/src/review2/BruteForce_1436.java
@@ -0,0 +1,28 @@
+package review2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BruteForce_1436 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(br.readLine());
+		br.close();
+		
+		int num=666;
+		int count=1;
+		while(count!=N) {
+			num++;
+			if(String.valueOf(num).contains("666")) {
+				count++;
+			}
+		}
+		
+		System.out.println(num);
+
+	}
+
+}
