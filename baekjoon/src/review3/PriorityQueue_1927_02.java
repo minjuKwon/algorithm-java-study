ce33efb77ed26de7db4d91b8ea912a8db542e81b
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2024-08-19 21:44:20 +0900

    Baekjoon PriorityQueue_1927_02
    study baekjoon algorithm : PriorityQueue_1927_02

---
+++ baekjoon/src/review3/PriorityQueue_1927_02.java
@@ -0,0 +1,66 @@
+package review3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PriorityQueue_1927_02 {
+
+	static int [] heap=new int[100_0001];
+	static int size=0;
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(br.readLine());
+		
+		StringBuilder sb=new StringBuilder();
+		while(N-->0) {
+			int x=Integer.parseInt(br.readLine());
+			if(x==0) {
+				if(size==0) sb.append(0).append('\n');
+				else sb.append(pop()).append('\n');
+			}else {
+				push(x);
+			}
+		}
+		br.close();
+
+		System.out.println(sb);
+		
+	}
+	
+	static void swap(int a, int b) {
+		int temp=heap[a];
+		heap[a]=heap[b];
+		heap[b]=temp;
+	}
+	
+	static void push(int num) {
+		heap[++size]=num;
+		int p=size;
+		while(p>1&&heap[p]<heap[p/2]) {
+			swap(p,p/2);
+			p/=2;
+		}
+	}
+
+	static int pop() {
+		int result=heap[1];
+		heap[1]=heap[size];
+		size--;
+		for(int i=1;i*2<=size;) {
+			if(heap[i]<heap[i*2]&&heap[i]<heap[i*2+1]) {
+				break; 
+			}else if(heap[i*2]>heap[i*2+1]) {
+				swap(i,i*2+1);
+				i=i*2+1;
+			}else {
+				swap(i,i*2);
+				i=i*2;
+			}
+		}
+		return result;
+	}
+	
+}
