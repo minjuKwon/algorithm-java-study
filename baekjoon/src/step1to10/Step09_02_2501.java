791f3f51ae6b041f7d491b2dee12de3dd2da47ee
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-03-02 19:17:08 +0900

    Baekjoon Step09_02_2501
    study baekjoon algorithm : Step09_02_2501

---
+++ baekjoon/src/step1to10/Step09_02_2501.java
@@ -0,0 +1,35 @@
+package step1to10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Step09_02_2501 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		int N=Integer.parseInt(st.nextToken());
+		int K=Integer.parseInt(st.nextToken());
+		br.close();
+		
+		int answer=0;
+		int count=1;
+		
+		for(int i=1;i<=N;i++) {
+			if(N%i==0) {
+				if(count==K) {
+					answer=i;
+					break;
+				}
+				count++;
+			}
+		}
+		
+		System.out.println(answer);
+
+	}
+
+}
