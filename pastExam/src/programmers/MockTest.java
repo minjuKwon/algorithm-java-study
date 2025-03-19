be8c90adf588478f57a411124a87e2b90eb3d1f7
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-03-20 01:24:44 +0900

    Programmers MockTest

---
+++ pastExam/src/programmers/MockTest.java
@@ -0,0 +1,40 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MockTest {
+
+	public static void main(String[] args) {
+		
+		int[] answers= {1,2,3,4,5};
+		
+		int [] p1={1, 2, 3, 4, 5};
+        int [] p2={2, 1, 2, 3, 2, 4, 2, 5};
+        int [] p3={3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int [] score=new int[3];
+        
+        for(int i=0;i<answers.length;i++){
+            if(answers[i]==p1[i%5]) score[0]++;
+            if(answers[i]==p2[i%8]) score[1]++;
+            if(answers[i]==p3[i%10]) score[2]++;
+        }
+       
+        int max=score[0];
+        for(int i=1;i<=2;i++){
+            if(max<score[i]) max=score[i];
+        }
+        
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i=0;i<3;i++){
+            if(max==score[i]) list.add(i+1);
+        }
+        
+       int[] answer=new int[list.size()]; 
+       for(int i=0;i<list.size();i++) answer[i]=list.get(i);
+
+       System.out.println(Arrays.toString(answer));
+       
+	}
+
+}
