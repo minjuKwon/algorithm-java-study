fa45ecd9cf63620a3c6ed92a2ca3a2e5f8f455b7
minjuKwon <95838547+minjuKwon@users.noreply.github.com>
2025-09-14 20:40:31 +0900

    Baekjoon String_11719
    study baekjoon algorithm : String_11719

---
+++ baekjoon/src/string/String_11719.java
@@ -0,0 +1,23 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class String_11719 {
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		String str="";
+		
+		while((str=br.readLine())!=null) {
+			System.out.println(str);
+		}
+		
+		br.close();
+
+	}
+
+}
